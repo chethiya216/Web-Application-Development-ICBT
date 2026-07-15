0a769f0e2c08b1dacd3c97ba14f8149867a724c7
chethiya216 <chethiyalakshan216@gmail.com>
2026-07-15 21:35:33 +0530

    Added boostrap to headers and footers

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<head>
+    <!-- Bootstrap 5.3 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Optional: Bootstrap Icons (Useful for cart, search, user icons) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
