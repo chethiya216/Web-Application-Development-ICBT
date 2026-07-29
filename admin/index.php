6cbc65fdc3c99af4cb05383d2634eca29d7b21ea
chethiya216 <chethiyalakshan216@gmail.com>
2026-07-29 13:28:13 +0530

    added missing template content

--- admin/index.php
+++ admin/index.php
@@ -482,7 +482,7 @@
               <ul class="navbar-nav flex-row align-items-center ms-auto">
                 <!-- Place this tag where you want the button to render. -->
                 <li class="nav-item lh-1 me-3">
-                  <a
+                  <!-- <a
                     class="github-button"
                     href="https://github.com/themeselection/sneat-html-admin-template-free"
                     data-icon="octicon-star"
@@ -490,7 +490,7 @@
                     data-show-count="true"
                     aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
                     >Star</a
-                  >
+                  > -->
                 </li>
 
                 <!-- User -->
@@ -504,12 +504,12 @@
                     <li>
                       <a class="dropdown-item" href="#">
                         <div class="d-flex">
-                          <div class="flex-shrink-0 me-3">
+                          <div class="shrink-0 me-3">
                             <div class="avatar avatar-online">
                               <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
                             </div>
                           </div>
-                          <div class="flex-grow-1">
+                          <div class="grow">
                             <span class="fw-semibold d-block">John Doe</span>
                             <small class="text-muted">Admin</small>
                           </div>
